e9b4c012ef5b29c9ef2b2f0a990e2f0b90eab7b8
xRetry <marcoschwarz.at@gmail.com>
2025-09-24 20:09:01 +0200

    Add npm package

--- plugin.js
+++ plugin.js
@@ -1,13 +1,20 @@
-// Register multiple UI elements
+import {
+    PluginAPI,
+    PluginManifest,
+    PluginHooks,
+} from '@super-productivity/plugin-api';
+
 PluginAPI.registerHeaderButton({
-  label: 'My Button',
-  icon: 'star',
-  onClick: async () => {
-    const tasks = await PluginAPI.getTasks();
-    console.log(`You have ${tasks.length} tasks`);
-  },
+    label: 'My Button',
+    icon: 'star',
+    onClick: async () => {
+        const tasks = await PluginAPI.getTasks();
+        console.log(`You have ${tasks.length} tasks`);
+    },
 });
 
 PluginAPI.registerHook(PluginAPI.Hooks.TASK_COMPLETE, (taskId) => {
-  console.log(`Task ${taskId} completed!`);
+    console.log(`Task ${taskId} completed!`);
 });
+
+
