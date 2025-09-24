9458982c6a1dc61c79927c28b67a4947e5906e7f
Schwarz Marco <Schwarz.Marco@voestalpine.com>
2025-09-24 21:23:49 +0200

    Fix inital script

--- plugin.js
+++ plugin.js
@@ -1,20 +1,42 @@
-import {
-    PluginAPI,
-    PluginManifest,
-    PluginHooks,
-} from '@super-productivity/plugin-api';
+/**
+ * @typedef {import('@super-productivity/plugin-api').PluginAPI} PluginAPI
+ * @typedef {import('@super-productivity/plugin-api').Task} Task
+ */
 
-PluginAPI.registerHeaderButton({
-    label: 'My Button',
+/** @type {PluginAPI} */
+const Api = PluginAPI;
+
+Api.registerHeaderButton({
+    label: 'Synchronize',
     icon: 'star',
     onClick: async () => {
-        const tasks = await PluginAPI.getTasks();
-        console.log(`You have ${tasks.length} tasks`);
+        await sync();
     },
 });
 
-PluginAPI.registerHook(PluginAPI.Hooks.TASK_COMPLETE, (taskId) => {
+Api.registerHook(Api.Hooks.TASK_COMPLETE, (taskId) => {
     console.log(`Task ${taskId} completed!`);
 });
 
+async function sync() {
+    const endpoint = await Api.loadSyncedData('endpoint');
 
+    if (endpoint == null) {
+        Api.showSnack({
+            msg: 'No endpoint defined!',
+            type: 'ERROR',
+        });
+        return;
+    }
+
+    const tasks = await Api.getTasks();
+    const resp = await fetch(endpoint, { method: 'post', body: JSON.stringify(tasks)});
+
+    if (!resp.ok) {
+        Api.showSnack({
+            msg: 'Sync failed: Response not ok!',
+            type: 'ERROR',
+        });
+        return;
+    }
+}
